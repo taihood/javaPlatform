a4d3082acf57d52f58d7918127845620a806c5da
guanjin <15322979@qq.com>
2017-12-16 11:07:15 +0800

    project creation2

--- src/main/java/com/yang/one/ReadingListController.java
+++ src/main/java/com/yang/one/ReadingListController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/readingList")
+@RequestMapping("/one")
 public class ReadingListController {
 
   private static final String reader = "craig";
--- src/test/java/com/yang/one/ReadinglistTests.java
+++ src/test/java/com/yang/one/ReadinglistTests.java
@@ -1,16 +1,36 @@
 package com.yang.one;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReadinglistTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	private MockMvc mvc;
 
+	@Autowired
+	private ReadingListRepository repository;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new ReadingListController(repository)).build();
+	}
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/one").accept(MediaType.TEXT_HTML))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn();
+	}
 }
